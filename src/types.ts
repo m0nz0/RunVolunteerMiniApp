bfffedccb782dfae68714e023011d46317b40972
mikekar <mikekar@ya.ru>
2025-08-22 18:03:44 +0300

    types

--- src/types.ts
+++ src/types.ts
@@ -62,3 +62,86 @@ interface Window {
         WebApp: WebApp;
     };
 }
+
+export interface GlobalLocationDictItem {
+    verstId: number;
+    name: string;
+}
+
+export interface UserLocationDictItem extends GlobalLocationDictItem {
+    cityName: string,
+    isFavorite: boolean,
+    isHome: boolean,
+    isDirected: boolean,
+    isRequested: boolean,
+    verstActive: boolean,
+    verstStatusName: string,
+    verstStatusCode: string,
+    botActive: boolean,
+    url: string,
+    lat: number,
+    lon: number,
+    href: string,
+    defaultPositions: DefautPosition,
+    directorTgIds: number[],
+    locationFlags: string[]
+}
+
+export interface PositionDict {
+    id: string;
+    name: string;
+    locationId: string;
+}
+
+export interface FlagChecker {
+    id: LocationFlag,
+    flag: boolean,
+
+}
+
+export interface CalendarData {
+    dates: Calendar[],
+    locations: UserLocationDictItem[]
+    location: UserLocationDictItem,
+    schedules: Team[]
+}
+
+export interface PositionData {
+    positions: Position[],
+    calendar: Calendar,
+    locations: UserLocationDictItem[]
+    team: Team[]
+}
+
+export interface Team {
+    id: number,
+    location: UserLocationDictItem,
+    locatinId: number,
+    calendar: Calendar,
+    calendarId: number,
+    position: Position,
+    positionId: number,
+    name: string,
+    tgUserId: number,
+    verstId: number,
+    tgUser: any
+}
+
+export interface Position {
+    id: number,
+    name: string,
+    isDEfault: boolean,
+    parentId?: number,
+    positionType: PositionType
+}
+
+export interface Calendar {
+
+    id: number;
+    date: string;
+    datetime: string
+    eventStatus: string,
+    isAdditional: boolean,
+}
+
+
