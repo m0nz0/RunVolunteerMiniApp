778ba7fd100621f01bbbc530be30ce4e7484a098
mikekar <mikekar@ya.ru>
2025-09-19 23:45:23 +0300

    positions level

--- src/Components/Team/TeamComponent.tsx
+++ src/Components/Team/TeamComponent.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useEffect, useState} from "react";
-import {TeamData} from "@/types";
+import {Position, TeamData} from "@/types";
 import {useParams} from "react-router-dom";
 import TeamService from "../../Services/TeamService";
 import {Button, ButtonGroup, ListGroup, Spinner} from "react-bootstrap";
@@ -11,9 +11,11 @@ import {toast} from "react-toastify";
 import {UserCardComponent} from "@/Components/UserCard/UserCardComponent";
 import {useUserContext} from "@/Common/Context/UserContext";
 import {getTelegramUser} from "@/Common/TelegramHelper";
+import {useGlobalContext} from "@/Common/Context/GlobalContext";
 
 export const TeamComponent: FC = () => {
     const [team, setTeam] = useState<TeamData>()
+    const {positionDict} = useGlobalContext();
     const [loading, setLoading] = useState<boolean>(true);
     const {updateUserDates} = useUserContext();
     const {locationId, calendarId} = useParams<{ locationId: string; calendarId: string }>();
@@ -61,6 +63,43 @@ export const TeamComponent: FC = () => {
                 team?.schedules.map(s => s.positionId).some(s => s === p.id));
     }
 
+    const test =
+        Object.entries(
+            Object.groupBy((team?.positions ?? [])
+                    .filter(p => p.positionType == PositionType.Main ||
+                        team?.schedules.map(s => s.positionId).some(s => s === p.id)),
+                (item: Position, index: number) => item.parent_id ?? item.id))
+            .map(([key, values]) => {
+                return {
+                    position: positionDict[Number(key)],
+                    users: team?.schedules.filter(s => s.positionId === Number(key)),
+                    subPositions: (values ?? []).length == 1 ? [] : (values ?? [])
+                        .map((sp) => {
+                            return {
+                                position: sp,
+                                users: team?.schedules.filter(s => s.positionId === sp.id),
+                            }
+                        })
+                }
+            })
+            .sort((a, b) => {
+                // ключ=1 всегда первый
+                if (a?.position?.id === 1) {
+                    return -1
+                }
+
+                if (b?.position?.id === 1) return 1;
+
+                let namea = team?.positions.find(x => x.id === Number(a?.position?.id))?.name
+                let nameb = team?.positions.find(x => x.id === Number(b?.position?.id))?.name
+
+                if (namea && nameb) {
+                    return namea.localeCompare(nameb)
+                }
+                return 1;
+            })
+
+
     let canSchedule = !team?.schedules.some(x => x.tgUser.tgId === userId) && !team?.hasOtherLocations
 
     let canTrySaveNrms = team?.verstAthlete?.id != null
@@ -92,7 +131,7 @@ export const TeamComponent: FC = () => {
                 Команда локации {team?.location?.name} за {DateService.formatDayMonthNameYear(team?.date?.date ?? "")}
             </h5>
         </p>
-        <p>
+        <div style={{"display": "flex", "justifyContent": "space-between"}}>
             <NameWithBadgeComponent name={"Записалось волонтёров"}
                                     badgeColor="success"
                                     badgeValue={(team?.schedules ?? []).length}
@@ -101,34 +140,41 @@ export const TeamComponent: FC = () => {
                                     badgeColor={"danger"}
                                     badgeValue={(thisPositions() ?? []).filter(p => !team?.schedules.some(s => s.positionId === p.id)).length}
                                     isRight={true}/>
-        </p>
+            <NameWithBadgeComponent name={"Частично закрыто позиций"}
+                                    badgeColor={"warning"}
+                                    badgeValue={(test ?? []).filter(p => p.subPositions.length > 0 && !p.subPositions.every(sp => sp.users?.length == 0)).length}
+                                    isRight={true}/>
+        </div>
         <ListGroup>
-            {(thisPositions() ?? [])
-                .sort((a, b) => {
-                    if (a.id === 1) return -1; // ключ=1 всегда первый
-                    if (b.id === 1) return 1;
+            {(test ?? [])
+                .map((x, index, array) => {
 
-                    let namea = team?.positions.find(x => x.id === Number(a.id))?.name
-                    let nameb = team?.positions.find(x => x.id === Number(b.id))?.name
-
-                    if (namea && nameb) {
-                        return namea.localeCompare(nameb)
+                    // danger success warning
+                    let rootColor = "success";
+                    if (x.subPositions.length == 0) {
+                        if (x.users?.length == 0) {
+                            rootColor = "danger";
+                        }
+                    } else {
+                        if (x.subPositions.every(sp => sp.users?.length == 0)) {
+                            rootColor = "danger"
+                        } else {
+                            rootColor = "warning"
+                        }
                     }
-                    return 1;
-                })
-                .map((position) => {
-                    let positionUsers = team?.schedules.filter(s => s.positionId === position.id) ?? [];
                     return <ListGroup.Item
-                        key={position.id}
+                        key={x.position.id}
                         as={"li"}
                         className="justify-content-between">
+
                         <div>
-                            <NameWithBadgeComponent name={position.name}
-                                                    badgeValue={positionUsers.length}
-                                                    badgeColor={positionUsers.length === 0 ? "danger" : "success"}/>
-                            {positionUsers
+                            <NameWithBadgeComponent name={x.position.name}
+                                                    badgeValue={x?.users?.length}
+                                                    badgeColor={rootColor}/>
+                            {(x.users ?? [])
                                 .map(u => {
-                                    return <div style={{"display": "flex", "alignItems": "baseline"}}>
+                                    return <div key={x.position.id + "-" + u.verstId}
+                                                style={{"display": "flex", "alignItems": "baseline"}}>
                                         <UserCardComponent key={u.id}
                                                            name={u.name}
                                                            verstId={u.verstId}
@@ -142,6 +188,35 @@ export const TeamComponent: FC = () => {
                                     </div>;
                                 })
                             }
+                            {(x.subPositions ?? []).map((sp) =>
+                                <div key={x.position.id + "-" + sp.position.id}
+                                     style={{"paddingLeft": "20px", "paddingTop": "5px"}}>
+                                    <NameWithBadgeComponent name={sp.position.name}
+                                                            badgeValue={sp?.users?.length}
+                                                            badgeColor={sp?.users?.length === 0 ? "danger" : "success"}/>
+                                    {(sp.users ?? [])
+                                        .map(u => {
+                                            return <div key={x.position.id + "-" + sp.position.id + "-" + u.verstId}
+                                                        style={{
+                                                            "display": "flex",
+                                                            "alignItems": "baseline",
+                                                            "paddingLeft": "20px"
+                                                        }}>
+                                                <UserCardComponent key={u.id}
+                                                                   name={u.name}
+                                                                   verstId={u.verstId}
+                                                                   tgLogin={u.tgUser.tgLogin}/>
+                                                {team?.canDelete && <Button variant={"danger"}
+                                                                            className={"py-1"}
+                                                                            onClick={async () => {
+                                                                                await deleteItem(u.id)
+                                                                            }}
+                                                                            disabled={disabled}>Удалить</Button>}
+                                            </div>;
+                                        })
+                                    }
+                                </div>
+                            )}
                         </div>
                     </ListGroup.Item>
                 })
@@ -154,6 +229,6 @@ export const TeamComponent: FC = () => {
                 {canTrySaveNrms && AppButtons.AuthNrms(Number(locationId), Number(calendarId))}
             </ButtonGroup>
         </ListGroup>
-        {/*todo сохранение roaster*/}
+        {/*<pre>{JSON.stringify(test, null, 2)}</pre>*/}
     </div>
 }
